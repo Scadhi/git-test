11fd4dd1d23f940a71277d00a5bf1fae9da4518c
Scathach <22125201@zju.edu.cn>
2022-08-19 21:15:26 +0800

    fisrt commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @Description: TODO
+ * @Author: Scathach
+ * @Aate: 2022/8/19 21:12
+ * @version: V1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
